add091f85002d1e046b77f87146d8933f9fbd4ec
v-v-v79 <vivedesh@gmail.com>
2022-09-12 22:39:06 -0700

    day21 edited

---
+++ src/Sleep10sec.java
@@ -0,0 +1,16 @@
+public class Sleep10sec {
+
+    static void sleep10sec() /*throws InterruptedException*/{
+        //Thread.sleep(3000);
+        System.out.println("Sleep for 3 seconds");
+        throw new ArithmeticException("Program was interrupted for 3 seconds");
+    }
+
+    public static void main(String args[]) {
+        //try {
+            sleep10sec();
+        //} catch (InterruptedException e) {
+            //e.printStackTrace();
+        //}
+    }
+}
