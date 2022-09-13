27956d86fe133cd4a48c167460a4f9a9e3af7c11
v-v-v79 <vivedesh@gmail.com>
2022-09-13 01:30:50 -0700

    abstraction day43 comment

--- src/leetcode/palindrome/Solution1.java
+++ src/leetcode/palindrome/Solution1.java
@@ -4,13 +4,25 @@ import java.util.Scanner;
 
 public class Solution1 {
     public static boolean isPalindrome(int x) {
-        if (x < 10)
+        if(x < 0)
+            return false;
+        if(x < 10)
             return true;
-        int y = x % 10;
-        int z = x - (10 * y);
-        if (y == z)
-            return true;
-        return false;
+        String str = "" + x;
+        char[] arr = str.toCharArray();
+        int j = arr.length - 1;
+        boolean isP = false;
+        for (int i = 0; i < arr.length / 2; i++) {
+            if(arr[i] == arr[j]){
+                j--;
+                isP = true;
+            }
+            else {
+                isP = false;
+                break;
+            }
+        }
+        return isP;
     }
 
     public static void main(String[] args){
